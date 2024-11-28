6434e911994ed9fc53333677397629ae93d41ec9
gopi6966 <gopivaraprasad17@gmail.com>
2024-11-28 14:35:35 +0530

    only sendig java file

--- src/package1/GitTest.java
+++ src/package1/GitTest.java
@@ -5,6 +5,13 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is created for the git purpose");
+int i =3;
+int j =5;
+add(i,j);
+	}
+	public static void add(int a,int b) {
+		int addition = a+b;
+		System.out.println(addition);
 	}
 
 }
